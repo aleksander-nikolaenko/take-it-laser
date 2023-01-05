141771169f064d97ad3afbb0eb90a9074d4a6793
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-01-05 12:49:14 +0200

    add button to top

--- js/nav-links.js
+++ js/nav-links.js
@@ -2,12 +2,17 @@ export const navLinkActions = () => {
   const anchors = document.querySelectorAll('a[href*="#"]');
   const sectionList = document.querySelectorAll(".section");
   const header = document.querySelector(".header");
+  const toTopBtn = document.querySelector("#to-top");
 
   const headerHeight = header.clientHeight;
 
   window.addEventListener("scroll", () => {
     const scrollDistance = window.scrollY;
     let currentSectionId = "";
+    // console.log(headerHeight);
+    scrollDistance > headerHeight
+      ? toTopBtn.classList.add("show")
+      : toTopBtn.classList.remove("show");
 
     sectionList.forEach((item) => {
       if (item.offsetTop - headerHeight - 10 <= scrollDistance) {
