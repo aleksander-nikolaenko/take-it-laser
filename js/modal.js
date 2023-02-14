6b2aaec85ca9def3bec209138a7f03722f21b686
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-14 13:24:36 +0200

    add input mask for user-phone

--- js/modal.js
+++ js/modal.js
@@ -27,6 +27,56 @@ export const modal = () => {
     Poltava: "Полтава",
   };
 
+  const mask = (selector) => {
+    let setCursorPosition = (pos, elem) => {
+      elem.focus();
+
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        let range = elem.createTextRange();
+
+        range.collapse = true;
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+      }
+    };
+
+    function createMask(event) {
+      let matrix = "+38__________",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+
+      if (def.length >= val.length) {
+        val = def;
+      }
+
+      this.value = matrix.replace(/./g, (a) => {
+        return /[_\d]/.test(a) && i < val.length
+          ? val.charAt(i++)
+          : i >= val.length
+          ? ""
+          : a;
+      });
+
+      if (event.type === "blur") {
+        if (this.value.length == 2) {
+          this.value = "";
+        }
+      } else {
+        setCursorPosition(this.value.length, this);
+      }
+    }
+
+    let input = document.querySelector(selector);
+
+    input.addEventListener("input", createMask);
+    input.addEventListener("blur", createMask);
+    input.addEventListener("focus", createMask);
+  };
+
   const openHandler = () => {
     modalBackdrop.classList.remove("is-hidden");
     document.querySelector("html").classList.add("no-scroll");
@@ -100,6 +150,8 @@ export const modal = () => {
       closeHandler();
     });
   };
+
+  mask('[name="user-tel"]');
 
   // Open modal when click to open button
   openModalBtn.forEach((item) => {
